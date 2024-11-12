e1913d676120d662930a4fc7f3c7fcf6ffaffa62
Nolulamo <ntshuma959@gmail.com>
2024-11-12 08:42:39 +0200

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js,css}', // Adjust these paths as necessary
+    './public/index.html',
+    './components/**/*.{html,js}',
+    './pages/**/*.{html,js}',
+    './index.html',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#008080',
+        secondary: '#FF6F61',
+        background: '#F5F5F5',
+      },
+      height:{
+        box: '189.6px',
+        boxlg:'230px',
+      }
+    },
+  },
+  plugins: [],
+}
+
